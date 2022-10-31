fc35411513706e86ac0b8884eb0b8c29a657977d
Adesina Adebisi <adeboysina@gmail.com>
2022-10-31 03:57:54 +0100

    Slack and github logo added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from "react";
 import "./App.css";
 import face from "./resources/face-card.png";
+import slack from "./resources/image.png"
+import github from "./resources/github.png"
 const App = () => {
   return (
     <>
       <div className="whole">
         <div className="view">
           <img alt="" src={face} width={200} id="profile_img" />{" "}
-          <p id="twitter">twitter: @rex_rachid</p>
+          <p id="twitter">twitter link: @rex_rachid</p>
           <p id="slack">AdebisiRex</p>
           <hr />
           <div className="anchor">
             <a href="https://training.zuri.team/" id="btn_zuri">
-              Training
+              Zuri Team
             </a>
             <a href="https://books.zuri.team/" id="books">
-              Books
+              Zuri Books
             </a>
             <div>
               <a title="This is a the easiest approach you'll find to learning Python
@@ -23,37 +25,47 @@ const App = () => {
                 href="https://books.zuri.team/python-for-beginners?ref_id=AdebisiRex"
                 id="btn_zuri"
               >
-                BuyPython
+                Python Books
               </a>
-              {/* <div className="subtext">
+              <div className="subtext">
                 This is a the easiest approach you'll find to learning Python
                 Programing on the market
-              </div> */}
+              </div>
             </div>
             <div>
               <a title="We offer an exclusive clearance to all our verified coders who
                 have passed through extensive check-ups by our dexterous team,
                 get yours?" href="https://background.zuri.team/" id="pitch">
-                Pitch
+                background check for coders
               </a>
-              {/* <div className="subtext">
+              <div className="subtext">
                 {" "}
                 We offer an exclusive clearance to all our verified coders who
                 have passed through extensive check-ups by our dexterous team,
                 get yours?{" "}
-              </div> */}
+              </div>
             </div>
             <div>
               <a title="Zuri is a team of verified proffesionals and we have this
                 perfectly curate book for learning designs to ease your path" href="https://books.zuri.team/design-rules" id="bood_design">
-                Zuri Design
+                Design Books 
               </a>
-              {/* <div className="subtext">
+              <div className="subtext">
                 Zuri is a team of verified proffesionals and we have this
                 perfectly curate book for learning designs to ease your path
-              </div> */}
+              </div>
+
             </div>
           </div>
+
+          <section>
+            <a href="https://slack.com/AdebisiRex"><img src={slack} alt="" width={35} /></a>
+            <a href="https://github.com/AdebisiRex/HNG-Linktree-Profile.git"><img src={github} alt="" width={35} /></a>
+          </section>
+
+          <footer>
+            
+          </footer>
         </div>
       </div>
     </>
